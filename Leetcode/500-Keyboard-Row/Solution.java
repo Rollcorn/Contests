0fcbbb76b02fc367475aa091fb380313a5eb57c4
denis shkandyuk <lazydaimon@gmail.com>
2022-03-25 17:45:41 +0300

    Solve Task #500

--- Leetcode/500-Keyboard-Row/Solution.java
+++ Leetcode/500-Keyboard-Row/Solution.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 
 /**
  * 500. Keyboard Row
@@ -7,26 +10,57 @@
  */
 
 class Solution {
-    String[] stringRow = new String[]{"qwertyuiop", "asdfghjkl", "zxcvbnm"};
 
-    public String[] findWords(String[] words) {
-        String[] result;
-
-        // Перебираем ряды символов 
-        for ( String strRow : stringRow ) {
-            char[] charRow = strRow.toString().toCharArray();    
-            // Перебираем слова 
-            for (String currWordStr : words ) {
-                char[] currWordChar = currWordStr.toString().toCharArray();    
-
-                // Ищем в ряду символов все буквы данного слова
-                for (int i = 0; i != charRow.length; i++ ){
-
-                }
-
+    public boolean isWordBelongToSet( HashSet<Character> set, String word ){
+        for (int i = 1; i < word.length(); ++i ){
+            if( !set.contains(word.charAt(i)) ){
+                return false;
             }
         }
+        return true;
+    }
 
-        return result;
+    public String[] findWords(String[] words) {
+        ArrayList<String> result = new ArrayList<>();
+
+        HashSet<Character> firstRow = new HashSet<>(Arrays.asList('q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'));
+        HashSet<Character> secondRow = new HashSet<>(Arrays.asList('a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'));
+        HashSet<Character> thirdRow = new HashSet<>(Arrays.asList('z', 'x', 'c', 'v', 'b', 'n', 'm'));
+
+        for ( String currentWord : words ){
+            String currentWordInLowCase = currentWord.toLowerCase();
+            Character firstCharInWord = currentWordInLowCase.charAt(0);
+            if( ( firstRow.contains(firstCharInWord) && isWordBelongToSet(firstRow, currentWordInLowCase) ) ||
+                ( secondRow.contains(firstCharInWord) && isWordBelongToSet(secondRow, currentWordInLowCase) ) ||
+                ( thirdRow.contains(firstCharInWord) && isWordBelongToSet(thirdRow, currentWordInLowCase) ) ) {
+                    result.add(currentWord);
+            }
+        }
+        return result.toArray(new String[0]);
+    }
+
+    public static void printArr(String[] str){
+        System.out.print("[");
+        for(String w : str ){
+            System.out.print(w + ", ");
+        }
+        System.out.println("]");
+    }
+
+    public static void main(String[] args) {
+
+        String[] str1 = {"Hello","Alaska","Dad","Peace"};
+        String[] str2 = {"adsdf","sfd"};
+        String[] str3 = {"omk"};
+
+        Solution s = new Solution();
+        String[] res1 = s.findWords(str1);
+        String[] res2 = s.findWords(str2);
+        String[] res3 = s.findWords(str3);
+        
+        printArr(res1);
+        printArr(res2);
+        printArr(res3);
+
     }
 }
