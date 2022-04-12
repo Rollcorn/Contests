3cf9156ac9de6a68527803ec171fbb8223d06b0f
denis shkandyuk <lazydaimon@gmail.com>
2022-04-12 17:55:20 +0300

    first commit

---
+++ LeetCode/144-Binary-Tree-Preorder-Traversal/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * Task Given the root of a binary tree, return the preorder traversal of its nodes' values.
+ */ 
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        
+    }
+}
